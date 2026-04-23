b060a8f3ffd9c543a876c2a282129d0d3bf9ae2c
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-04-23 21:53:05 +0300

    Generics mixins

--- src/generics/index.ts
+++ src/generics/index.ts
@@ -150,3 +150,44 @@
 
   const resp2 = new HttpResp<string>('data', 0);
 }
+
+// Mixins
+{
+  type Constructor = new (...args: any[]) => {};
+
+  type GConstructor<T = {}> = new (...args: any[]) => T;
+
+  class List {
+    constructor(public items: string[]) {}
+  }
+
+  class Accordion {
+    isOpen: boolean;
+  }
+
+  type ListType = GConstructor<List>;
+  type AccordionType = GConstructor<Accordion>;
+
+  class ExtendedListClass extends List {
+    first(): string | undefined {
+      return this.items[0];
+    }
+  }
+
+  function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
+    return class extends Base {
+      first(): string | undefined {
+        return this.items[0];
+      }
+    };
+  }
+
+  class AccordionList {
+    isOpen: boolean;
+    constructor(public items: string[]) {}
+  }
+
+  const List1 = ExtendedList(AccordionList);
+  const list1Instance = new List1(['1', '2']);
+  console.log(list1Instance.first());
+}
