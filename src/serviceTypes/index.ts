44fd14b4bb5c55199c67a725e21a7fe68dde4358
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-05-02 16:20:29 +0300

    service types

--- src/serviceTypes/index.ts
+++ src/serviceTypes/index.ts
@@ -44,3 +44,50 @@
   // взять из union только те которые НЕ расширяют string
   type ExcludeEx = Exclude<'from' | 'to' | Payment, string>;
 }
+
+// ReturnType, Parameters, ConstructorParameters
+{
+  class User {
+    constructor(
+      public id: number,
+      public name: string,
+    ) {}
+  }
+
+  function getData(id: number): User {
+    return new User(1, 'Вася');
+  }
+
+  type RT = ReturnType<typeof getData>;
+  type RT2 = ReturnType<() => void>;
+  type RT3 = ReturnType<<T>() => T>; // unknown
+  type RT4 = ReturnType<<T extends string>() => T>; // string
+
+  type PT = Parameters<typeof getData>;
+  type FirstPT = PT[0];
+
+  type CP = ConstructorParameters<typeof User>;
+  type IT = InstanceType<typeof User>;
+}
+
+// Awaited
+{
+  type A = Awaited<Promise<string>>;
+  type A2 = Awaited<Promise<Promise<string>>>;
+
+  interface IMenu {
+    name: string;
+    url: string;
+  }
+
+  async function getMenu(): Promise<IMenu[]> {
+    return [{ name: '123', url: '123/' }];
+  }
+
+  type R = Awaited<ReturnType<typeof getMenu>>;
+
+  async function getArray<T>(params: T) {
+    // Promise<Awaited<T>[]>
+    return [await params];
+  }
+}
