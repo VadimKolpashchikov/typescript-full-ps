fe4e2e6420063ac93c3bd5c0d954306e7929f806
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-05-04 22:00:01 +0300

    ts decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -20,14 +20,14 @@
   }
 
   function logUsersCount(obj: IUserService) {
-    console.log(`Users: ${obj.usersCount}`);
+    // console.log(`Users: ${obj.usersCount}`);
 
     return obj;
   }
 
-  console.log(new UserService().getUsersCount());
-  console.log(nullUser(new UserService()).getUsersCount());
-  console.log(logUsersCount(nullUser(new UserService())).getUsersCount());
+  // console.log(new UserService().getUsersCount());
+  // console.log(nullUser(new UserService()).getUsersCount());
+  // console.log(logUsersCount(nullUser(new UserService())).getUsersCount());
 }
 
 // Class decorators
@@ -59,7 +59,7 @@
     };
   }
 
-  console.log(new UserService().getUsersCount());
+  // console.log(new UserService().getUsersCount());
 }
 
 // Decorator factory
@@ -93,7 +93,7 @@
     };
   }
 
-  console.log(new UserService().getUsersCount());
+  // console.log(new UserService().getUsersCount());
 }
 
 // Class decorators TEST
@@ -122,7 +122,7 @@
     };
   }
 
-  console.log((new UserService() as IUserService & CreatedAt).createdAt);
+  // console.log((new UserService() as IUserService & CreatedAt).createdAt);
 }
 
 // Method decorators
@@ -149,7 +149,7 @@
       (this: This, ...args: Args) => Return
     >,
   ): (this: This, ...args: Args) => number {
-    console.log(context.name);
+    // console.log(context.name);
 
     return function (this: This, ...args: Args) {
       return 123;
@@ -171,7 +171,7 @@
           return target.apply(this, args);
         } catch (e) {
           if (e instanceof Error) {
-            console.log('Опа, ошибочка из декоратора: ' + e.message);
+            // console.log('Опа, ошибочка из декоратора: ' + e.message);
 
             if (rethrow) {
               throw e;
@@ -182,7 +182,7 @@
     };
   }
 
-  console.log(new UserService().getUsersCount());
+  // console.log(new UserService().getUsersCount());
 }
 
 // Property decorators
@@ -208,14 +208,14 @@
     ): ClassAccessorDecoratorResult<This, Value> {
       return {
         get(this: This): Value {
-          console.log(`Чтение свойства ${String(context.name)}`);
+          // console.log(`Чтение свойства ${String(context.name)}`);
           return target.get.call(this);
         },
         set(this: This, value: Value) {
           if (value > max) {
-            console.log(
-              `Нельзя установить значение больше ${max}. Установлено: ${max}`,
-            );
+            // console.log(
+            //   `Нельзя установить значение больше ${max}. Установлено: ${max}`,
+            // );
             target.set.call(this, max as Value);
           } else {
             target.set.call(this, value);
@@ -226,7 +226,87 @@
   }
   const service = new UserService();
   service.usersCount = 1000;
-  console.log(service.usersCount);
+  // console.log(service.usersCount);
   service.usersCount = 10;
-  console.log(service.usersCount);
+  // console.log(service.usersCount);
+}
+
+// Decorator order
+{
+  function Uni(name: string): any {
+    // console.log(`Initialization ${name}`);
+    return function () {
+      // console.log(`Call ${name}`);
+    };
+  }
+
+  @Uni('class')
+  class MyClass {
+    @Uni('property')
+    prop?: any;
+
+    @Uni('static property')
+    static prop2?: any;
+
+    @Uni('static method')
+    static method(_: any) {}
+
+    @Uni('method')
+    method(_: any) {}
+
+    constructor(_: any) {}
+  }
+}
+
+// Decorators >5.x.x
+{
+  @ClassDec
+  class Demo {
+    // @MethodDec
+    @Max(10)
+    exec(a: number) {
+      console.log(a);
+    }
+  }
+
+  function MethodDec<This, Args extends any[], Return>(
+    target: (this: This, ...args: Args) => Return,
+    context: ClassMethodDecoratorContext<
+      This,
+      (this: This, ...args: Args) => Return
+    >,
+  ) {
+    return function (this: This, ...args: Args): Return {
+      const res = target.apply(this, args);
+
+      return res;
+    };
+  }
+
+  function Max(num: number) {
+    return function <This, Args extends any[], Return>(
+      target: (this: This, ...args: Args) => Return,
+      context: ClassMethodDecoratorContext<
+        This,
+        (this: This, ...args: Args) => Return
+      >,
+    ) {
+      return function (this: This, ...args: Args): Return {
+        if (args[0] > num) {
+          throw new Error(`Значение не может быть больше ${num}`);
+        }
+        const res = target.apply(this, args);
+
+        return res;
+      };
+    };
+  }
+
+  function ClassDec<This, Args extends any[]>(
+    target: new (...args: Args) => This,
+    context: ClassDecoratorContext<new (...args: Args) => This>,
+  ) {}
+
+  const demo = new Demo();
+  demo.exec(123);
 }
