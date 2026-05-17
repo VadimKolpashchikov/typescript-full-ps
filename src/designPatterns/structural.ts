bf45787b33de4fcc869847231ea287b45861ee0a
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-05-17 14:22:08 +0300

    structural patterns

--- src/designPatterns/structural.ts
+++ src/designPatterns/structural.ts
@@ -182,3 +182,65 @@
   const proxy2 = new PaymentAccessProxy(new PaymentApi(), 3);
   console.log(proxy2.getPaymentDetails(1));
 }
+
+// Composite
+{
+  abstract class DeliveryItem {
+    items: DeliveryItem[] = [];
+
+    addItem(item: DeliveryItem) {
+      this.items.push(item);
+    }
+
+    protected getItemsPrice(): number {
+      return this.items.reduce(
+        (acc: number, item: DeliveryItem) => acc + item.getPrice(),
+        0,
+      );
+    }
+
+    abstract getPrice(): number;
+  }
+
+  class DeliveryShop extends DeliveryItem {
+    constructor(private deliveryFee: number) {
+      super();
+    }
+
+    getPrice(): number {
+      return this.getItemsPrice() + this.deliveryFee;
+    }
+  }
+
+  class Package extends DeliveryItem {
+    getPrice(): number {
+      return this.getItemsPrice();
+    }
+  }
+
+  class Product extends DeliveryItem {
+    constructor(private price: number) {
+      super();
+    }
+
+    getPrice(): number {
+      return this.price;
+    }
+  }
+
+  const shop = new DeliveryShop(100);
+
+  shop.addItem(new Product(1000));
+  const pack1 = new Package();
+  pack1.addItem(new Product(100));
+  pack1.addItem(new Product(300));
+  shop.addItem(pack1);
+
+  shop.addItem(new Product(555000));
+  const pack2 = new Package();
+  pack2.addItem(new Product(5000));
+  pack2.addItem(new Product(500));
+  shop.addItem(pack2);
+
+  console.log(shop.getPrice());
+}
